81c9be1351a382324bb749dbcb07eecc88bf560f
torbalansky <torbalansky@gmail.com>
2023-10-08 16:36:51 +0100

    Task 1.7 Part 3. Added Function 3: search_by_ingredients(), Function 4: edit_recipe() and Function 5: delete_recipe()

--- task1.7/recipe_app.py
+++ task1.7/recipe_app.py
@@ -1,5 +1,5 @@
 # Import necessary modules from SQLAlchemy
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, or_, and_
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Create an SQLAlchemy engine to connect to the MySQL DB
@@ -124,4 +124,256 @@ def view_all_recipes(session):
         return
     
     for recipe in recipes:
+        print("+" * 40)
         print(recipe)
+
+def search_by_ingredients(session):
+    try:
+        # Check if there are any entries in the table
+        if session.query(Recipe).count() == 0:
+            print("No recipes found in the database.")
+            return
+
+        # Retrieve values from the 'ingredients' column
+        results = session.query(Recipe.ingredients).all()
+
+        all_ingredients = set()
+        for result in results:
+            ingredients_list = result[0].split(', ')
+            all_ingredients.update(ingredients_list)
+
+        # Display the available ingredients to the user
+        print("Available ingredients:")
+        for index, ingredient in enumerate(all_ingredients, start=1):
+            print(f"{index}. {ingredient}")
+
+        # Ask the user for ingredient choices
+        ingredient_choices = input("Enter ingredient numbers separated by spaces: ").split()
+        
+        # Convert user input to integers
+        ingredient_choices = [int(choice) for choice in ingredient_choices]
+
+        # Validate user input
+        if not all(1 <= choice <= len(all_ingredients) for choice in ingredient_choices):
+            print("Invalid ingredient choice. Please enter valid numbers.")
+            return
+
+        # Get the selected ingredients as strings
+        search_ingredients = [list(all_ingredients)[choice - 1] for choice in ingredient_choices]
+
+        # Initialize a list of search conditions
+        conditions = []
+        for ingredient in search_ingredients:
+            like_term = f"%{ingredient}%"
+            conditions.append(Recipe.ingredients.like(like_term))
+
+        # Query the database for recipes matching the conditions
+        matching_recipes = session.query(Recipe).filter(*conditions).all()
+
+        if not matching_recipes:
+            print("No recipes found matching the selected ingredients.")
+        else:
+            print("Matching Recipes:")
+            for recipe in matching_recipes:
+                print(recipe)
+
+    except Exception as e:
+        print_result_message(success=False, message="An error occurred while searching for recipes:", exception=e)
+
+def search_by_ingredients(session):
+    try:
+        # Check if there are any entries in the table
+        if session.query(Recipe).count() == 0:
+            print("No recipes found in the database.")
+            return
+
+        # Retrieve values from the 'ingredients' column
+        results = session.query(Recipe.ingredients).all()
+
+        all_ingredients = set()
+        for result in results:
+            ingredients_list = result[0].split(', ')
+            all_ingredients.update(ingredients_list)
+
+        # Display the available ingredients to the user
+        print("Available ingredients:")
+        for index, ingredient in enumerate(all_ingredients, start=1):
+            print(f"{index}. {ingredient}")
+
+        # Ask the user for ingredient choices
+        ingredient_choices = input("Enter ingredient numbers separated by spaces: ").split()
+        
+        # Convert user input to integers
+        ingredient_choices = [int(choice) for choice in ingredient_choices]
+
+        # Validate user input
+        if not all(1 <= choice <= len(all_ingredients) for choice in ingredient_choices):
+            print("Invalid ingredient choice. Please enter valid numbers.")
+            return
+
+        # Get the selected ingredients as strings
+        search_ingredients = [list(all_ingredients)[choice - 1] for choice in ingredient_choices]
+
+        # Initialize a list of search conditions for at least one ingredient
+        any_conditions = []
+        for ingredient in search_ingredients:
+            like_term = f"%{ingredient}%"
+            any_conditions.append(Recipe.ingredients.like(like_term))
+
+        # Query the database for recipes containing at least one selected ingredient
+        any_matching_recipes = session.query(Recipe).filter(or_(*any_conditions)).all()
+
+        # Initialize a list of search conditions for every ingredient
+        all_conditions = []
+        for ingredient in search_ingredients:
+            like_term = f"%{ingredient}%"
+            all_conditions.append(Recipe.ingredients.like(like_term))
+
+        # Query the database for recipes containing every selected ingredient
+        all_matching_recipes = session.query(Recipe).filter(and_(*all_conditions)).all()
+
+        print("\nRecipes containing at least one selected ingredient:")
+        if any_matching_recipes:
+            for recipe in any_matching_recipes:
+                print(recipe)
+        else:
+            print("No recipes found containing at least one selected ingredient.")
+
+        print("\nRecipes containing every selected ingredient:")
+        if all_matching_recipes:
+            for recipe in all_matching_recipes:
+                print(recipe)
+        else:
+            print("No recipes found containing every selected ingredient.")
+
+    except Exception as e:
+        print_result_message(success=False, message="An error occurred while searching for recipes:", exception=e)
+
+def edit_recipe(session):
+    try:
+        # Check if any recipes exist in the DB
+        recipes = session.query(Recipe).all()
+        if not recipes:
+            print("No recipes were found.")
+            return
+        
+        # Display the list of recipes to choose from
+        print("\nRecipes")
+        for i, recipe in enumerate(recipes, start=1):
+            print(f"{i}. {recipe.name}")
+        
+        # Ask the user to select a recipe by number
+        choice = input("Enter the number of the recipe to edit (or 'q' to quit): ")
+        if choice.lower() == 'q':
+            return
+        
+        try: 
+            choice = int(choice)
+            if 1 <= choice <= len(recipes):
+                # Retrieve the selected recipe
+                recipe_to_edit = recipes[choice - 1]
+
+                # Display the selected recipe details
+                print("\nSelected Recipe: ")
+                print(recipe_to_edit)
+
+                # Ask the user which attribute to edit
+                attribute = input("\nEnter the number of the attribute to edit:"
+                                  "\n1. Name\n2. Ingredients\n3. Cooking Time\n"
+                                  "Or press 'q' to quit editing: ")
+                
+                if attribute.lower() == 'q':
+                    return
+                
+                try:
+                    attribute = int(attribute)
+                    if attribute == 1:
+                        # Edit recipe name
+                        new_name = input("Enter the new name of the recipe: ")
+                        if len(new_name) > 50:
+                            print("Recipe name is too long (max 50 characters).")
+                        else:
+                            recipe_to_edit.name = new_name
+                            session.commit()
+                            print_result_message(success=True)
+                    elif attribute == 2:
+                        # Edit ingredients
+                        num_ingredients = int(input("Enter the number of ingredients: "))
+                        new_ingredients = []
+                        for i in range(num_ingredients):
+                            ingredient = input(f"Enter ingredient {i + 1}: ")
+                            new_ingredients.append(ingredient)
+                        recipe_to_edit.ingredients = ", ".join(new_ingredients)
+                        
+                        # Recalculate difficulty after updating ingredients
+                        recipe_to_edit.calculate_difficulty()
+                        
+                        session.commit()
+                        print_result_message(success=True)
+                    elif attribute == 3:
+                        new_cooking_time = input("Enter the new cooking time in minutes: ")
+                        try:
+                            new_cooking_time = int(new_cooking_time)
+                            recipe_to_edit.cooking_time = new_cooking_time
+                            recipe_to_edit.calculate_difficulty()
+                            session.commit()
+                            print_result_message(success=True)
+                        except ValueError:
+                            print("Invalid cooking time. Please enter a valid number.")
+                    else:
+                        print("Invalid attribute choice. Please enter a valid number (1, 2, or 3).")
+                except ValueError:
+                    print("Invalid attribute choice. Please enter a valid number (1, 2, or 3).")
+            else:
+                print("Invalid recipe choice. Please enter a valid number.")
+        except ValueError:
+            print("Invalid recipe choice. Please enter a valid number.")
+
+    except Exception as e:
+        print_result_message(success=False, message="There was an error editing the recipe:", exception=e)
+
+def delete_recipe(session):
+    try:
+        # Check the available recipes in the DB
+        recipes = session.query(Recipe).all()
+        if not recipes:
+            print("No recipes found in the database.")
+            return
+
+        # Display a list of recipes to choose from
+        print("\nRecipes:")
+        for i, recipe in enumerate(recipes, start=1):
+            print(f"{i}. {recipe.name}")
+
+        # Ask the user to select a recipe by number
+        choice = input("Enter the number of the recipe to delete (or 'q' to quit): ")
+        if choice.lower() == 'q':
+            return
+
+        try:
+            choice = int(choice)
+            if 1 <= choice <= len(recipes):
+                # Retrieve the selected recipe
+                recipe_to_delete = recipes[choice - 1]
+
+                # Display the selected recipe's details
+                print("\nSelected Recipe:")
+                print(recipe_to_delete)
+
+                # Ask for confirmation
+                confirm = input("Are you sure you want to delete this recipe? (yes/no): ").lower()
+                if confirm == "yes":
+                    session.delete(recipe_to_delete)
+                    session.commit()
+                    print_result_message(success=True)
+                elif confirm == "no":
+                    print("Recipe deletion canceled.")
+                else:
+                    print("Invalid choice. Please enter 'yes' or 'no'.")
+            else:
+                print("Invalid recipe choice. Please enter a valid number.")
+        except ValueError:
+            print("Invalid recipe choice. Please enter a valid number.")
+
+    except Exception as e:
+        print_result_message(success=False, message="There was an error deleting the recipe:", exception=e)
